d3780032b158ce12671f60062e75c2788b216cd1
sshklarov91 <sergeshklarov@gmail.com>
2022-09-27 19:51:46 -0400

    second commit

--- src/git01B14/git01B14.java
+++ src/git01B14/git01B14.java
@@ -4,7 +4,7 @@ public class git01B14 {
 
 	public static void main(String[] args) {
 	System.out.println("This is a git class");
-
+	System.out.println("hey this is a change in my project");
 	}
 
 }
