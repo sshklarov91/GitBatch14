0933dbe381b904ecc3d37f496a3f0100bf3c5fc1
sshklarov91 <sergeshklarov@gmail.com>
2022-09-27 20:07:25 -0400

    third commit

--- src/git01B14/git01B14.java
+++ src/git01B14/git01B14.java
@@ -5,6 +5,7 @@ public class git01B14 {
 	public static void main(String[] args) {
 	System.out.println("This is a git class");
 	System.out.println("hey this is a change in my project");
+	System.out.println("how are u this a new version");
 	}
 
 }
